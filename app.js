15471738240ac44bc9e6d8612e05e6742eb867f7
Ting Zhou <limtingzhou@gmail.com>
2018-12-22 17:56:09 +0800

    Add functionality of adding subscription to MailChimp using POST request

--- app.js
+++ app.js
@@ -18,5 +18,38 @@ app.post("/", function(req, res) {
   let firstName = req.body.fName;
   let lastName = req.body.lName;
   let email = req.body.email;
+
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+      }
+    ]
+  }
+
+  let jsonData = JSON.stringify(data);
+
   console.log(email);
+
+  let options = {
+    url: "https://us7.api.mailchimp.com/3.0/lists/67d60097a8",
+    method: "POST",
+    headers: {
+      "Authorization": "tingzhou 25d4a71757d77fbd9631eab84fde3e0f-us7",
+    },
+    body: jsonData,
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response.statusCode);
+    }
+  });
 });
+
+
+// 25d4a71757d77fbd9631eab84fde3e0f-us7
+// 67d60097a8
