b27c5c568795348530ed7e4afeb859c8901df6cf
Ting Zhou <limtingzhou@gmail.com>
2018-12-22 17:35:45 +0800

    Add bootstrap template and setup localhost server

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const request = require("request");
 const app = new express();
-
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
   console.log(`Server is running on port ${3000}`);
+});
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+  console.log(email);
 });
